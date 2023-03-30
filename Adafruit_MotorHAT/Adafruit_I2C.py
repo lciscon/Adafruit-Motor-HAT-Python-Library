e36ccc0b354df636c8b2dd21f0ab1e0261535f0a
lciscon <lciscon@yahoo.com>
2023-03-30 12:46:28 -0500

    Updated files to Python3

--- Adafruit_MotorHAT/Adafruit_I2C.py
+++ Adafruit_MotorHAT/Adafruit_I2C.py
@@ -48,9 +48,9 @@ class Adafruit_I2C(object):
     self.address = address
     # By default, the correct I2C bus is auto-detected using /proc/cpuinfo
     # Alternatively, you can hard-code the bus version below:
-    # self.bus = smbus.SMBus(0); # Force I2C0 (early 256MB Pi's)
-    # self.bus = smbus.SMBus(1); # Force I2C1 (512MB Pi's)
-    self.bus = smbus.SMBus(busnum if busnum >= 0 else Adafruit_I2C.getPiI2CBusNumber())
+    #self.bus = smbus.SMBus(0); # Force I2C0 (early 256MB Pi's)
+    self.bus = smbus.SMBus(1); # Force I2C1 (512MB Pi's)
+    #self.bus = smbus.SMBus(busnum if busnum >= 0 else Adafruit_I2C.getPiI2CBusNumber())
     self.debug = debug
 
   def reverseByteOrder(self, data):
@@ -64,7 +64,7 @@ class Adafruit_I2C(object):
     return val
 
   def errMsg(self):
-    print "Error accessing 0x%02X: Check your I2C address" % self.address
+    print("Error accessing 0x%02X: Check your I2C address" % self.address)
     return -1
 
   def write8(self, reg, value):
@@ -72,8 +72,8 @@ class Adafruit_I2C(object):
     try:
       self.bus.write_byte_data(self.address, reg, value)
       if self.debug:
-        print "I2C: Wrote 0x%02X to register 0x%02X" % (value, reg)
-    except IOError, err:
+        print("I2C: Wrote 0x%02X to register 0x%02X" % (value, reg))
+    except(IOError):
       return self.errMsg()
 
   def write16(self, reg, value):
@@ -83,7 +83,7 @@ class Adafruit_I2C(object):
       if self.debug:
         print ("I2C: Wrote 0x%02X to register pair 0x%02X,0x%02X" %
          (value, reg, reg+1))
-    except IOError, err:
+    except(IOError):
       return self.errMsg()
 
   def writeRaw8(self, value):
@@ -91,18 +91,18 @@ class Adafruit_I2C(object):
     try:
       self.bus.write_byte(self.address, value)
       if self.debug:
-        print "I2C: Wrote 0x%02X" % value
-    except IOError, err:
+        print("I2C: Wrote 0x%02X" % value)
+    except(IOError):
       return self.errMsg()
 
   def writeList(self, reg, list):
     "Writes an array of bytes using I2C format"
     try:
       if self.debug:
-        print "I2C: Writing list to register 0x%02X:" % reg
-        print list
+        print("I2C: Writing list to register 0x%02X:" % reg)
+        print(list)
       self.bus.write_i2c_block_data(self.address, reg, list)
-    except IOError, err:
+    except(IOError):
       return self.errMsg()
 
   def readList(self, reg, length):
@@ -112,9 +112,9 @@ class Adafruit_I2C(object):
       if self.debug:
         print ("I2C: Device 0x%02X returned the following from reg 0x%02X" %
          (self.address, reg))
-        print results
+        print(results)
       return results
-    except IOError, err:
+    except(IOError):
       return self.errMsg()
 
   def readU8(self, reg):
@@ -125,7 +125,7 @@ class Adafruit_I2C(object):
         print ("I2C: Device 0x%02X returned 0x%02X from reg 0x%02X" %
          (self.address, result & 0xFF, reg))
       return result
-    except IOError, err:
+    except(IOError):
       return self.errMsg()
 
   def readS8(self, reg):
@@ -137,7 +137,7 @@ class Adafruit_I2C(object):
         print ("I2C: Device 0x%02X returned 0x%02X from reg 0x%02X" %
          (self.address, result & 0xFF, reg))
       return result
-    except IOError, err:
+    except(IOError):
       return self.errMsg()
 
   def readU16(self, reg, little_endian=True):
@@ -149,9 +149,9 @@ class Adafruit_I2C(object):
       if not little_endian:
         result = ((result << 8) & 0xFF00) + (result >> 8)
       if (self.debug):
-        print "I2C: Device 0x%02X returned 0x%04X from reg 0x%02X" % (self.address, result & 0xFFFF, reg)
+        print("I2C: Device 0x%02X returned 0x%04X from reg 0x%02X" % (self.address, result & 0xFFFF, reg))
       return result
-    except IOError, err:
+    except(IOError):
       return self.errMsg()
 
   def readS16(self, reg, little_endian=True):
@@ -160,12 +160,12 @@ class Adafruit_I2C(object):
       result = self.readU16(reg,little_endian)
       if result > 32767: result -= 65536
       return result
-    except IOError, err:
+    except(IOError):
       return self.errMsg()
 
 if __name__ == '__main__':
   try:
     bus = Adafruit_I2C(address=0)
-    print "Default I2C bus is accessible"
+    print("Default I2C bus is accessible")
   except:
-    print "Error accessing default I2C bus"
+    print("Error accessing default I2C bus")
--- examples/DCTest.py
+++ examples/DCTest.py
@@ -17,7 +17,7 @@ def turnOffMotors():
 atexit.register(turnOffMotors)
 
 ################################# DC motor test!
-myMotor = mh.getMotor(3)
+myMotor = mh.getMotor(1)
 
 # set the speed to start, from 0 (off) to 255 (max speed)
 myMotor.setSpeed(150)
@@ -27,32 +27,32 @@ myMotor.run(Adafruit_MotorHAT.RELEASE);
 
 
 while (True):
-	print "Forward! "
+	print("Forward! ")
 	myMotor.run(Adafruit_MotorHAT.FORWARD)
 
-	print "\tSpeed up..."
+	print("\tSpeed up...")
 	for i in range(255):
 		myMotor.setSpeed(i)
 		time.sleep(0.01)
 
-	print "\tSlow down..."
+	print("\tSlow down...")
 	for i in reversed(range(255)):
 		myMotor.setSpeed(i)
 		time.sleep(0.01)
 
-	print "Backward! "
+	print("Backward! ")
 	myMotor.run(Adafruit_MotorHAT.BACKWARD)
 
-	print "\tSpeed up..."
+	print("\tSpeed up...")
 	for i in range(255):
 		myMotor.setSpeed(i)
 		time.sleep(0.01)
 
-	print "\tSlow down..."
+	print("\tSlow down...")
 	for i in reversed(range(255)):
 		myMotor.setSpeed(i)
 		time.sleep(0.01)
 
-	print "Release"
+	print("Release")
 	myMotor.run(Adafruit_MotorHAT.RELEASE)
 	time.sleep(1.0)
